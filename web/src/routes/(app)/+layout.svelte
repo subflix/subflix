e5188ce9b3d0454965ad60c73c030515710360ad
Pakorn Sangpeth <camel4882@gmail.com>
2023-06-25 23:13:59 +0800

    feat: sidebar can theme

--- web/src/routes/(app)/+layout.svelte
+++ web/src/routes/(app)/+layout.svelte
@@ -1,10 +1,14 @@
 <script>
+	import Navigation from '$lib/components/Navigation/Navigation.svelte'
 	import { AppShell } from '@skeletonlabs/skeleton'
 </script>
 
-<AppShell>
+<AppShell slotSidebarLeft="bg-surface-500/50 border-r border-primary-500/10 w-56 p-4">
 	<svelte:fragment slot="header">Header</svelte:fragment>
-	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<Navigation />
+	</svelte:fragment>
+
 	<slot />
 	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
 </AppShell>
