fcc18972e841766e524ae04e79fa300e06863f79
Pakorn Sangpeth <camel4882@gmail.com>
2023-06-25 22:44:31 +0800

    feat: add fonts

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '@fontsource/noto-sans-thai'
+	import '@fontsource/noto-sans-thai/200.css'
+	import '@fontsource/noto-sans-thai/300.css'
+	import '@fontsource/noto-sans-thai/400.css'
+	import '@fontsource/noto-sans-thai/500.css'
+	import '@fontsource/noto-sans-thai/600.css'
+	import '@fontsource/noto-sans-thai/700.css'
+	import '@fontsource/noto-sans-thai/800.css'
+	import '../app.postcss'
+</script>
+
+<slot />
+
+<svelte:head>
+	<title>SubFlix</title>
+</svelte:head>
